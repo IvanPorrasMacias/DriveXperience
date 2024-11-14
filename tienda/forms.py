1d6d0ffdc8e195a56df89c55d513460e5c115d49
IvanPorrasMacias <158307571+IvanPorrasMacias@users.noreply.github.com>
2024-11-14 10:52:02 -0600

    UPDATE forms.py
    Se actualizan los campos solicitados para coincidir con el modelado del Usuario

--- tienda/forms.py
+++ tienda/forms.py
@@ -5,7 +5,7 @@ from .models import Usuario
 class CustomUserRegistrationForm(UserCreationForm):
     class Meta:
         model = Usuario
-        fields = ['username', 'password1', 'password2']
+        fields = ['first_name', 'last_name', 'direccion', 'teléfono', 'RFC', 'password1', 'password2']
 
 class CustomUserAuthenticationForm(AuthenticationForm):
     pass
